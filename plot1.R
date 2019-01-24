add7208738645db561f9fc2b96ea64146eab6d73
abdoghareeb46 <abdo.ghareeb50@gmail.com>
2019-01-24 18:26:17 +0200

    edit

--- plot1.R
+++ plot1.R
@@ -16,14 +16,3 @@ png(file="plot1.png", height=480, width=480)
 dev.off()
 
 
-
-
-## Getting full dataset
-ig <- read.csv("household_power_consumption.txt", header=T, sep=';', na.strings="?", 
-               nrows=2075259, check.names=F, stringsAsFactors=F, comment.char="", quote='\"')
-ig$Date <- as.Date(ig$Date, format="%d/%m/%Y")
-
-## Subsetting the data
-meow <- subset(ig, subset=(Date >= "2007-02-01" & Date <= "2007-02-02"))
-rm(ig)
-
